9ac66d307c73913ccba10a8c0712dbb36f1295a8
RottenPi <q297885252@163.com>
2019-07-03 12:09:03 +0800

    add

--- Source Code/AIDA_client/src/client/InteractWithServer.java
+++ Source Code/AIDA_client/src/client/InteractWithServer.java
@@ -12,17 +12,7 @@ import java.util.Vector;
 
 public final class InteractWithServer {
 
-    /**
-     * @Title: postToServer
-     * @Description:与服务器验证端口建立连接，并使用对象流传输数据
-     * @param: obj
-     *             发送给服务器的对象
-     * @return: result 接收服务器返回的对象信息
-     * @exception: IOException
-     *                 如果与服务器建立连接失败或创建对象输入输出流失败产生IOException.
-     * @exception: ClassNotFoundException
-     *                 如果对象输入流读取异常产生ClassNotFoundException.
-     */
+    // 与服务器验证端口建立连接，并使用对象流传输数据
     private static Object postToServer(Object obj) {
         Object result = null;
         try {
@@ -49,15 +39,7 @@ public final class InteractWithServer {
         return result;
     }
 
-    /**
-     * @Title: isLogin
-     * @Description:验证登录用户名或密码是否正确，发送构造的身份信息到服务器
-     * @param: userId
-     *             用户ID
-     * @param: userPassword
-     *             经过MD5加密后的密码
-     * @return: Object 返回接收的回馈对象(Boolean)
-     */
+    // 验证登录用户名或密码是否正确，发送构造的身份信息到服务器
     public static Object isLogin(String userId, String userPassword) {
         // 构造身份信息
         ChatVerify userInfo = new ChatVerify(userId, userPassword);
@@ -66,13 +48,7 @@ public final class InteractWithServer {
         return postToServer(userInfo);
     }
 
-    /**
-     * @Title: getUserInfo
-     * @Description:通过ID获取用户的信息，包括个人资料以及好友列表群列表
-     * @param: userId
-     *             用户ID
-     * @return: userInfo 包含用户信息的对象
-     */
+    // 通过ID获取用户的信息，包括个人资料以及好友列表群列表
     public static User getUserInfo(String userId) {
         User userInfo = null;
         String fieldString = "getUserInfo" + userId;
@@ -80,46 +56,25 @@ public final class InteractWithServer {
         return userInfo;
     }
 
-    /**
-     * @Title: getChatRecord
-     * @Description:通过交互双方id获取历史聊天记录
-     * @param: fromid
-     *             交互方1
-     * @param: toId
-     *             交互方2
-     * @param: isGroup
-     *             是否为群聊天记录
-     * @return: Vector<String> 返回聊天记录信息的Vector数组
-     */
-    @SuppressWarnings("unchecked")
+    // 通过交互双方id获取历史聊天记录
     public static Vector<String> getChatRecord(String fromid, String toId, boolean isGroup) {
         String sendString = "getChatRecord```" + fromid + "```" + toId + "```" + isGroup;
         return (Vector<String>) postToServer(sendString);
     }
 
-    /**
-     * @Title: getGroupMembers
-     * @Description:通过群ID向服务器发送请求，获取群所有成员ID
-     * @param: groupId
-     *             群ID
-     * @return: Vector<String> 返回群成员ID的Vector数组
-     */
-    @SuppressWarnings("unchecked")
+    // 通过群ID向服务器发送请求，获取群所有成员ID
     public static Vector<String> getGroupMembers(String groupId) {
         String send = "getGroupMembers" + groupId;
         return (Vector<String>) postToServer(send);
     }
 
-    /**
-     * @Title: setMyTrades
-     * @Description:向服务器发送请求更改个性签名
-     * @param: myId
-     *             需要修改的用户ID(只能是自己)
-     * @param: content
-     *             新的个性签名内容
-     * @return: void
-     */
+    // 向服务器发送请求更改个性签名
     public static void setMyTrades(String myId, String content) {
         postToServer("setMyTrades```" + myId + "```" + content);
     }
+
+    // 向服务器搜索好友
+
+    // 向服务器请求添加好友
+//    public \
 }
