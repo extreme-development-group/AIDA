111d3a87398ba9e1c0d65d4a74165a726b7513fb
RottenPi <q297885252@163.com>
2019-07-03 09:04:03 +0800

    add server code

---
+++ Source Code/MyServer/src/DataBaseConnection.java
@@ -0,0 +1,81 @@
+import com.mysql.cj.protocol.Resultset;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+
+public class DataBaseConnection {
+    // 驱动名及数据库url
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/oicq?useSSL=false&serverTimezone=UTC";
+    // 数据库用户名密码
+    static final String USER = "root";
+    static final String PASS = "779756800Tswcbyy";
+    // 连接对象
+    private Connection conn = null;
+    private PreparedStatement prestmt = null;
+
+    DataBaseConnection() {
+        try {
+            // 注册JDBC驱动
+            Class.forName(JDBC_DRIVER);
+            // 连接数据库
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("DataBase Connected Successfully!");
+    }
+
+    // 查询
+    public ResultSet getFromDataBase(String sql) {
+        ResultSet rs = null;
+        try {
+            prestmt = conn.prepareStatement(sql);
+            // 开始查询
+            rs = prestmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return rs;
+    }
+
+    // 其他增、删、改操作
+    public boolean updateDataBase(String sql) {
+        try {
+            prestmt = conn.prepareStatement(sql);
+            prestmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void close() {
+        try {
+            if (prestmt != null && !prestmt.isClosed()) {
+                prestmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn!=null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("DBConnection closed successfully!");
+    }
+
+    public static void main(String[] args) {
+        DataBaseConnection demo = new DataBaseConnection();
+    }
+}
---
+++ Source Code/MyServer/src/DataStream.java
@@ -0,0 +1,104 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+import java.util.Vector;
+
+public final class DataStream implements Runnable{
+    private DataInputStream in;
+    private DataOutputStream out;
+    private String userId;
+
+    // 利用与客户端连接的Socket对象创建数据输入输出流
+    DataStream(Socket clientSocket, String userId) {
+        this.userId = userId;
+        try {
+            in = new DataInputStream(clientSocket.getInputStream());
+            out = new DataOutputStream(clientSocket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("创建聊天数据传输流失败：" + e.getMessage());
+        }
+        sendToAllFriends("OnlineSituation```在线```" + userId);
+    }
+
+    // 通知该用户的所有好友(一般为上线信息或离线信息)
+    private void sendToAllFriends(String message) {
+        System.out.println(message);
+        //当前连接用户好友列表
+        Vector<String> friends;
+        friends = DataCheck.getFriendMember(userId);
+        for (int i = 0; i < friends.size(); i++) {
+            if (ServerListener.getClientUser().containsKey(friends.get(i))) {
+                ServerListener.getClientUser().get(friends.get(i)).send(message);
+            }
+        }
+    }
+
+    // 接收客户端发送来的请求
+    @Override
+    public void run() {
+        String scMessage;
+        try {
+            while (true) {
+                scMessage = in.readUTF();
+                // 处理消息
+                execute(scMessage);
+            }
+        } catch (IOException e) {
+            // 离线断开连接出错
+            // 离线后删除用户在线信息
+            ServerListener.getClientUser().remove(userId);
+
+            // 通知所有好友离线情况
+            sendToAllFriends("OnlineSituation```离线```" + userId);
+            System.out.println("用户" + userId + "的离线，剩余在线人数 ：" + ServerListener.getClientUser().size());
+        }
+    }
+
+    // 处理客户端所发送来的信息，将它转发给其他用户或群
+    private void execute(String message) {
+        String res[] = message.split("```", 4);
+        if (res.length == 4) {
+            /*
+             * res[0]：发送标识 、res[1]：fromId 、res[2]：toId 、res[3]：消息内容
+             */
+            String type = res[0];
+            String toId = res[2];
+            message = new Date().toString() + "```" + message;
+            if (type.equals("toFriend")) {
+                // 发送给好友的消息
+                if (ServerListener.getClientUser().containsKey(toId)) {
+                    ServerListener.getClientUser().get(toId).send(message);
+                    System.out.println(message);
+                } else {
+                    // 好友不在线的情况
+                }
+            } else if (type.equals("toGroup")) {
+                // 群聊消息
+                Vector<String> groups = DataCheck.getGroupMember(toId);
+                for (int i = 0; i < groups.size(); i++) {
+                    if (groups.get(i).equals(userId) == false
+                            && ServerListener.getClientUser().containsKey(groups.get(i))) {
+                        // 发给除了自己的其他人
+                        ServerListener.getClientUser().get(groups.get(i)).send(message);
+                    } else {
+                        // 群成员不在线情况
+                    }
+                }
+            }
+        } else {
+            System.out.println("收到的信息不规范：" + message);
+        }
+    }
+
+    // 发送消息到连接的客户端
+    private void send(String message) {
+        try {
+            out.writeUTF(message);
+        } catch (IOException e) {
+            System.out.println(userId + "发送到客户端失败：" + e.getMessage());
+        }
+    }
+
+}
